301f2bb4b4318b5b34fca28a921120f0267d69a3
Tabarek Ayad <tabarek.ayad@gmail.com>
2021-03-03 00:29:29 +0300

    menu button, javascript & rotate

---
+++ dist/js/main.js
@@ -0,0 +1,32 @@
+// Select DOM items
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+
+const navItem = document.querySelectorAll(".nav-items");
+
+// Set initial state
+
+let showMenu = false;
+
+menuBtn.addEventListener("click", ToggleMenu);
+
+function ToggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItem.forEach((item) => item.classList.add("show"));
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItem.forEach((item) => item.classList.remove("show"));
+    showMenu = false;
+  }
+}
