0e7d3ae00c7dca74f23ec1fdc32fdd47f804a098
Tabarek Ayad <tabarek.ayad@gmail.com>
2021-05-15 03:03:36 +0300

    refactor, changes to the style

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,7 +5,7 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 
-const navItem = document.querySelectorAll(".nav-items");
+const navItem = document.querySelectorAll(".nav-item");
 
 // Set initial state
 
@@ -14,19 +14,19 @@ let showMenu = false;
 menuBtn.addEventListener("click", ToggleMenu);
 
 function ToggleMenu() {
-  if (!showMenu) {
-    menuBtn.classList.add("close");
-    menu.classList.add("show");
-    menuNav.classList.add("show");
-    menuBranding.classList.add("show");
-    navItem.forEach((item) => item.classList.add("show"));
-    showMenu = true;
-  } else {
-    menuBtn.classList.remove("close");
-    menu.classList.remove("show");
-    menuNav.classList.remove("show");
-    menuBranding.classList.remove("show");
-    navItem.forEach((item) => item.classList.remove("show"));
-    showMenu = false;
-  }
+    if (!showMenu) {
+        menuBtn.classList.add("close");
+        menu.classList.add("show");
+        menuNav.classList.add("show");
+        menuBranding.classList.add("show");
+        navItem.forEach((item) => item.classList.add("show"));
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove("close");
+        menu.classList.remove("show");
+        menuNav.classList.remove("show");
+        menuBranding.classList.remove("show");
+        navItem.forEach((item) => item.classList.remove("show"));
+        showMenu = false;
+    }
 }
